e9a7f365472a32583de7cc8bd7c505d10d0b0f84
jasmaa <jasonmaa3955@gmail.com>
2019-06-09 09:26:04 -0400

    Improve shiritori, add logging to bot

--- bot.py
+++ bot.py
@@ -1,56 +1,94 @@
 import praw
-from shiritori import Game    
+import logging
+import sys
+import shiritori
+
+def build_response(content):
+    """Builds bot response
+    """
+    response = content
+    response += "\n***\n^(I am a bot and I byte)"
+    return response
+
+def check_replied(comment):
+    """Check if the bot has already replied in the comments
+    """
+    with open("reply_ids.txt", "r") as f:
+        reply_ids = set(f.read().split("\n"))
+
+    # check if comment already replied to
+    for c in comment.replies.list():
+        if c.id in reply_ids:
+            return True
+    return False
+    
 
 def process_comment(game, comment):
-    """Descends and processes replies"""
-    print(comment.body)
-    print(game.last_word)
+    """Descends and processes replies
+    """
 
     with open("reply_ids.txt", "r") as f:
         reply_ids = set(f.read().split("\n"))
 
     if comment.id not in reply_ids:
+
+        logging.info(f"Processing comment {comment.id}: {comment.body}")
+        
         try:
             g = game.copy()
             g.add_word(comment.body)
-            
         except Exception as error:
-
-            # check if comment already replied to
-            is_replied = False
-            for c in comment.replies.list():
-                if c.id in reply_ids:
-                    is_replied = True
-                    break
-
-            if not is_replied:
-                reply = comment.reply(f"GAME OVER: {error}")
-                with open("reply_ids.txt", "a") as f:
-                    f.write(f"{reply.id}\n")
+            # Process shiritori exception
+            if type(error) == shiritori.ShiritoriException:
+                if not check_replied(comment):
+                    reply = comment.reply(build_response(f"GAME OVER: {error}"))
+                    with open("reply_ids.txt", "a") as f:
+                        f.write(f"{reply.id}\n")
+                        
+            # Rethrow otherwise
+            else:
+                raise
     
     # DFS through comments
     for next_comment in comment.replies.list():
         process_comment(g, next_comment)
 
 def main():
-    """Main"""
+    """Main
+    """
+
+    # Logging
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    file_handler = logging.StreamHandler(open("log.txt", "w", encoding="utf-8"))
+    file_handler.setFormatter(formatter)
+    
+    root.addHandler(stdout_handler)
+    root.addHandler(file_handler)
+    
+    
+    logging.info("Starting bot...")
 
     reddit = praw.Reddit('shiritori-bot')
     subreddit = reddit.subreddit('test')
 
     while True:
-        #g.add_word(input("Enter word: "))
-        #print(g.last_word, g.is_alive)
 
         for submission in subreddit.new(limit=10):
             if submission.title == "SHIRITORI START":
-                print("SUBMISSION: " + submission.selftext)
+
+                logging.info(f"Processing submission {submission.id}: {submission.selftext}")
+                
                 for comment in submission.comments:
                     # ignore deleted
                     if comment.body == "[deleted]":
                         continue
                 
-                    g = Game()
+                    g = shiritori.Game()
                     g.add_word(submission.selftext)
                     process_comment(g, comment)
 
--- shiritori.py
+++ shiritori.py
@@ -1,5 +1,11 @@
 import requests
 import json
+import jaconv
+
+class ShiritoriException(Exception):
+    """Illegal shiritori move
+    """
+    pass
 
 class Game:
     """Shiritori Game
@@ -11,6 +17,8 @@ class Game:
         self.is_alive = True
 
     def copy(self):
+        """Deep copies game
+        """
         g = Game()
         g.last_word = self.last_word
         g.word_set = self.word_set.copy()
@@ -20,17 +28,24 @@ class Game:
     def add_word(self, word):
         """Adds word to chain
         """
+        
         if self.is_alive:
-            reading_data = self.get_word_data(word)
-            if reading_data:
-                self.last_word = reading_data
-                self.word_set.add(reading_data['word'])
+            word_data = self.get_word_data(word)
+            if word_data:
+                self.last_word = word_data
+
+                # Add word to set, otherwise default to add reading
+                if 'word' in word_data.keys():
+                    self.word_set.add(word_data['word'])
+                else:
+                    self.word_set.add(word_data['reading'])
             else:
                 self.is_alive = False
 
     def get_word_data(self, word):
         """Gets and validates word
         """
+        
         r = requests.get(f"https://jisho.org/api/v1/search/words?keyword={word}")
         data = json.loads(r.text)
 
@@ -39,23 +54,37 @@ class Game:
             word_data = data['data'][0]
 
             # Get reading of word
-            target = ""
+            target = None
             for possible in word_data['japanese']:
-                if ('word' in possible.keys() and possible['word'] == word) or possible['reading'] == word:
+                if ('word' in possible.keys() and possible['word'] == word) or ('reading' in possible.keys() and possible['reading'] == word):
                     target = possible
                     break
+                
+            # Validate and convert reading to hira
+            if target and 'reading' in target.keys():
+                target['reading'] = jaconv.kata2hira(target['reading'])
+            else:
+                raise ShiritoriException("Word is invalid")
+            
+            # Check if word ends in n
+            if target['reading'][-1] == "ん":
+                raise ShiritoriException("End with n")
+                
+            # Check if word on chain
+            if ('word' in target.keys() and target['word'] in self.word_set) or target['reading'] in self.word_set:
+                raise ShiritoriException("Word is already on the chain")
+                
+            # Check if word fits chain
+            if len(self.word_set) != 0 and target['reading'][0] != self.last_word['reading'][-1]:
+                raise ShiritoriException("Word does not fit on the chain")
+                    
+            return target
+                    
+        raise ShiritoriException("Word is invalid")
 
-            if len(target) > 0:
-                # Check if word ends in n
-                if target['reading'][-1] == "ん": # make it work with katakana
-                    raise Exception("End with n")
-                # Check if word on chain
-                if target['word'] in self.word_set:
-                    raise Exception("Word already on chain")
-                # Check if word fits chain
-                if len(self.word_set) != 0 and target['reading'][0] != self.last_word['reading'][-1]:
-                    raise Exception("Word does not fit on chain")
-                    
-                return target
-                    
-        raise Exception("Word is invalid")
+
+if __name__ == "__main__":
+    g = Game()
+    while True:
+        g.add_word(input("Enter word: "))
+        print(g.last_word)
